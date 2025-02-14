ba5f73f5a8b57ef15832d26c9d1ddf0e7c963615
ways92 <d.ferdiansya@gmail.com>
2025-02-15 00:00:55 +0700

    update navbar

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="bg-dark text-white text-center py-3 mt-4">
-    <p>&copy; {{ date('Y') }} To-Do List App. All Rights Reserved.</p>
+<footer class="footer">
+    <p>&copy; {{ date('Y') }} Goodtimes</p>
 </footer>

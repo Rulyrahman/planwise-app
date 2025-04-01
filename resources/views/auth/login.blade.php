751004fb63b66c1b761a927361996d7a4d6310d9
Ruly Rahman <d.ferdiansya@gmail.com>
2025-04-01 22:48:55 +0700

    register form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,23 +3,29 @@
 @section('title', 'login')
 
 @section('content')
-    <div class="login-form">
-        <h3>Login Here</h3>
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-            <label for="email">Email</label>
-            <input type="text" placeholder="Email" id="email" name="email" value="{{ old('email') }}" required>
+    <div class="login-blade">
+        <div class="btn-req-back">
+            <a href="/">BACK</a>
+            <a href="/register">REGS</a>
+        </div>
+        <div class="login-form">
+            <h3>Login Here</h3>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <label for="email">Email</label>
+                <input type="text" placeholder="Email" id="email" name="email" value="{{ old('email') }}" required>
 
-            <label for="password">Password</label>
-            <input type="password" placeholder="Password" id="password" name="password" required>
+                <label for="password">Password</label>
+                <input type="password" placeholder="Password" id="password" name="password" required>
 
-            <button class="login-btn" type="submit">Log In</button>
+                <button class="login-btn" type="submit">Log In</button>
 
-            <div class="social">
-                <div class="go"><i class="fab fa-google"></i> Google</div>
-                <div class="fb"><i class="fab fa-facebook"></i> Facebook</div>
-            </div>
+                <div class="social">
+                    <div class="go"><i class="fab fa-google"></i> Google</div>
+                    <div class="fb"><i class="fab fa-facebook"></i> Facebook</div>
+                </div>
 
+            </form>
             @if ($errors->any())
                 <div class="alert alert-danger mt-2">
                     <ul>
@@ -29,6 +35,6 @@
                     </ul>
                 </div>
             @endif
-        </form>
+        </div>
     </div>
 @endsection
